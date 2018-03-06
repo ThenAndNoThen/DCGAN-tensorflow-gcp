7982ac4b87a9125c7ede4bd23f01e732836b1c69
ThenAndNoThen <w86568376@163.com>
2018-03-06 17:56:26 +0800

    argument remove

--- task.py
+++ task.py
@@ -68,65 +68,65 @@ def run_experiment(hparams):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   # Input Arguments
-  parser.add_argument(
-      '--train-files',
-      help='GCS or local paths to training data',
-      nargs='+',
-      required=True
-  )
-  parser.add_argument(
-      '--num-epochs',
-      help="""\
-      Maximum number of training data epochs on which to train.
-      If both --max-steps and --num-epochs are specified,
-      the training job will run for --max-steps or --num-epochs,
-      whichever occurs first. If unspecified will run for --max-steps.\
-      """,
-      type=int,
-  )
-  parser.add_argument(
-      '--train-batch-size',
-      help='Batch size for training steps',
-      type=int,
-      default=40
-  )
-  parser.add_argument(
-      '--eval-batch-size',
-      help='Batch size for evaluation steps',
-      type=int,
-      default=40
-  )
-  parser.add_argument(
-      '--eval-files',
-      help='GCS or local paths to evaluation data',
-      nargs='+',
-      required=True
-  )
-  # Training arguments
-  parser.add_argument(
-      '--embedding-size',
-      help='Number of embedding dimensions for categorical columns',
-      default=8,
-      type=int
-  )
-  parser.add_argument(
-      '--first-layer-size',
-      help='Number of nodes in the first layer of the DNN',
-      default=100,
-      type=int
-  )
-  parser.add_argument(
-      '--num-layers',
-      help='Number of layers in the DNN',
-      default=4,
-      type=int
-  )
-  parser.add_argument(
-      '--scale-factor',
-      help='How quickly should the size of the layers in the DNN decay',
-      default=0.7,
-      type=float
-  )
+  # parser.add_argument(
+  #     '--train-files',
+  #     help='GCS or local paths to training data',
+  #     nargs='+',
+  #     required=True
+  # )
+  # parser.add_argument(
+  #     '--num-epochs',
+  #     help="""\
+  #     Maximum number of training data epochs on which to train.
+  #     If both --max-steps and --num-epochs are specified,
+  #     the training job will run for --max-steps or --num-epochs,
+  #     whichever occurs first. If unspecified will run for --max-steps.\
+  #     """,
+  #     type=int,
+  # )
+  # parser.add_argument(
+  #     '--train-batch-size',
+  #     help='Batch size for training steps',
+  #     type=int,
+  #     default=40
+  # )
+  # parser.add_argument(
+  #     '--eval-batch-size',
+  #     help='Batch size for evaluation steps',
+  #     type=int,
+  #     default=40
+  # )
+  # parser.add_argument(
+  #     '--eval-files',
+  #     help='GCS or local paths to evaluation data',
+  #     nargs='+',
+  #     required=True
+  # )
+  # # Training arguments
+  # parser.add_argument(
+  #     '--embedding-size',
+  #     help='Number of embedding dimensions for categorical columns',
+  #     default=8,
+  #     type=int
+  # )
+  # parser.add_argument(
+  #     '--first-layer-size',
+  #     help='Number of nodes in the first layer of the DNN',
+  #     default=100,
+  #     type=int
+  # )
+  # parser.add_argument(
+  #     '--num-layers',
+  #     help='Number of layers in the DNN',
+  #     default=4,
+  #     type=int
+  # )
+  # parser.add_argument(
+  #     '--scale-factor',
+  #     help='How quickly should the size of the layers in the DNN decay',
+  #     default=0.7,
+  #     type=float
+  # )
   parser.add_argument(
       '--job-dir',
       help='GCS location to write checkpoints and export models',
@@ -145,27 +145,27 @@ if __name__ == '__main__':
       ],
       default='INFO',
   )
-  # Experiment arguments
-  parser.add_argument(
-      '--train-steps',
-      help="""\
-      Steps to run the training job for. If --num-epochs is not specified,
-      this must be. Otherwise the training job will run indefinitely.\
-      """,
-      type=int
-  )
-  parser.add_argument(
-      '--eval-steps',
-      help='Number of steps to run evalution for at each checkpoint',
-      default=100,
-      type=int
-  )
-  parser.add_argument(
-      '--export-format',
-      help='The input format of the exported SavedModel binary',
-      choices=['JSON', 'CSV', 'EXAMPLE'],
-      default='JSON'
-  )
+  # # Experiment arguments
+  # parser.add_argument(
+  #     '--train-steps',
+  #     help="""\
+  #     Steps to run the training job for. If --num-epochs is not specified,
+  #     this must be. Otherwise the training job will run indefinitely.\
+  #     """,
+  #     type=int
+  # )
+  # parser.add_argument(
+  #     '--eval-steps',
+  #     help='Number of steps to run evalution for at each checkpoint',
+  #     default=100,
+  #     type=int
+  # )
+  # parser.add_argument(
+  #     '--export-format',
+  #     help='The input format of the exported SavedModel binary',
+  #     choices=['JSON', 'CSV', 'EXAMPLE'],
+  #     default='JSON'
+  # )
   parser.add_argument(
       '--input-height',
       type=int
